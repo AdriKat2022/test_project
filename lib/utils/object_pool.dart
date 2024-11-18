3456d46e1b29920b4d7f92626b31b462b20896b8
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-11-18 11:02:33 +0100

    added forgotten null check

--- lib/utils/object_pool.dart
+++ lib/utils/object_pool.dart
@@ -4,17 +4,18 @@ class ObjectPool<T extends PositionComponent>{
   final List<T> pool = [];
   final int maxSize;
   final T Function() createObjectFunction;
+  // Can be defined so this is executed 
   final void Function(T)? resetObjectFunction;
 
   ObjectPool({required this.maxSize, required this.createObjectFunction, this.resetObjectFunction = null});
 
   // Get an object from the pool (or create one)
-  T? getObject({Vector2? spawnPosition = null, bool resetObject = false}){
+  T? getObject({Vector2? spawnPosition = null, bool resetObject = true}){
 
     // Search for an available object in the pool (with the isMounted property)
     for(var object in pool){
       if(!object.isMounted){
-        if (resetObject) resetObjectFunction!(object);
+      if (resetObject && resetObjectFunction != null) resetObjectFunction!(object);
         if (spawnPosition != null) object.position = spawnPosition;
         return object;
       }
@@ -23,7 +24,7 @@ class ObjectPool<T extends PositionComponent>{
     // Create a new object if none is available
     if (pool.length < maxSize){
       final newObject = createObjectFunction();
-      if (resetObject) resetObjectFunction!(newObject);
+      if (resetObject && resetObjectFunction != null) resetObjectFunction!(newObject);
       if (spawnPosition != null) newObject.position = spawnPosition;
       pool.add(newObject);
       return newObject;
